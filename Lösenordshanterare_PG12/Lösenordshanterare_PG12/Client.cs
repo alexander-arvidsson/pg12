cdbad47998e3ecbbe4a1a47bb78005603c3984a1
Alexarv <Exopower43!>
2021-03-03 14:59:16 +0100

    simple json

---
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Lösenordshanterare_PG12
+{
+    public class Client
+    { 
+        public string User { get; set; }
+        public string Password { get; set; }
+
+    }
+
+}
---
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/JsonClient.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text.Json;
+using System.IO;
+using System;
+
+namespace Lösenordshanterare_PG12
+{
+    public class JsonClient
+    {
+        private Client jsonClient = new Client();
+
+       
+        public void GetUserAndPassword()
+        {
+            Console.WriteLine("Write your name: ");
+            jsonClient.User = Console.ReadLine();
+            Console.WriteLine("Write your password: ");
+            jsonClient.Password = Console.ReadLine();
+        }
+        public void StoreJsonClient() {
+            string jsonPass = JsonSerializer.Serialize(jsonClient);
+
+            File.WriteAllText("JsonClient", jsonPass);
+            Console.WriteLine(jsonPass);
+
+        }
+    }
+}
