2b07e3a744eade9026437eef5c676baed083d12d
Alexarv <Exopower43!>
2021-03-05 15:30:29 +0100

    Created init commands with respective properties

--- Lösenordshanterare_PG12/Lösenordshanterare_PG12/Client.cs
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Client.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using System;
+using System.IO;
 using System.Text.Json;
 
 namespace Lösenordshanterare_PG12
 {
-    public class Client
-    { 
-        public string User { get; set; }
-        public string Password { get; set; }
+    public class ClientObjects
+    {
+        public string SecretKey { get; set; }
 
+    }
+
+    public class Client
+    {
+        //Starting mPassword not sure if best left blank
+        public string masterPassword = "bestInTown1337";
+        ClientObjects objects = new ClientObjects();
+        private SecretKeyGenerator generator = new SecretKeyGenerator();
+
+        public void CreateClient()
+        {
+            objects.SecretKey = generator.GenerateSecretKey();
+            string storeJsonString = JsonSerializer.Serialize(objects);
+            File.WriteAllText("client.json", storeJsonString);
+            Console.WriteLine(storeJsonString);
+
+        }
     }
 
 }
--- Lösenordshanterare_PG12/Lösenordshanterare_PG12/Server.cs
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Server.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Security.Cryptography;
 
 namespace Lösenordshanterare_PG12
 {
-    class Server
+    public class ServerObjects
     {
+        public string IV { get; set; }
+        public string Vault { get; set; }
+
+    }
+
+    public class Server
+    {
+        public void CreateServer()
+        {
+            ServerObjects objects = new ServerObjects();
+            AesEncryptor aes = new AesEncryptor();
+            string unencryptedVault = "asdasd";
+
+            objects.IV = aes.GenerateIV();
+            objects.Vault = aes.EncryptVault(unencryptedVault, objects.IV);
+
+            string storeJsonString = JsonSerializer.Serialize(objects);
+
+            File.WriteAllText("server.json", storeJsonString);
+            //for testing purposes
+            Console.WriteLine(storeJsonString);
+        }
+
     }
 }
