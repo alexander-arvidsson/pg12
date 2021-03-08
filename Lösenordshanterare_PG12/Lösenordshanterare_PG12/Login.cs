25380eba050ac9efc500c9b4e29e938fd8edc4dc
lisafagerberg <79850031+lisafagerberg@users.noreply.github.com>
2021-03-08 15:45:23 +0100

    Meny påbörjad

--- Lösenordshanterare_PG12/Lösenordshanterare_PG12/Login.cs
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Login.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Lösenordshanterare_PG12
 {
     class Login //Borde klassen Server vara public?
     {
-        private JsonClient c = new JsonClient();
+        //private JsonClient c = new JsonClient();
         private Server s;
     }
 }
