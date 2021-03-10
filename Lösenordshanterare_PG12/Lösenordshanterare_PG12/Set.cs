5491e48b94c9652e6f51ce9a5531e2921675c37d
iv-so <74911828+iv-so@users.noreply.github.com>
2021-03-10 10:26:06 +0100

    set class

---
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Set.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Lösenordshanterare_PG12
+{
+    class Set
+    {
+        //Without error handling so far
+        Server server;
+
+        public static void set(string[] args)
+        {
+            //Decrypt Vault gives json representation of the correct vault?
+
+            string jsonVault = "decrypted vault json";
+
+            //Deserialize vault 
+            string newPassword = Console.ReadLine();
+            string prop = "AmazonUsername";     //given from somewhere
+            
+            JsonSerializer.Deserialize<Dictionary<string, object>>(jsonVault).Add(prop, newPassword);
+            
+
+
+        }
+    }
+}
