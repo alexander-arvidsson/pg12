b4efe30fbbc82fede64522e8a3f222e6817f60f2
iv-so <74911828+iv-so@users.noreply.github.com>
2021-03-15 17:35:21 +0100

    -g still needs work

--- Lösenordshanterare_PG12/Lösenordshanterare_PG12/Set.cs
+++ Lösenordshanterare_PG12/Lösenordshanterare_PG12/Set.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -12,24 +13,39 @@ namespace Lösenordshanterare_PG12
         Server s = new Server();
         RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
 
-        public void set(string property)
+        public void set(string property, string flag)
         {
             Console.WriteLine("Enter your master password");
             string mPassword = Console.ReadLine();
+            string alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                                  "abcdefghijklmnopqrstuvwxyz" +
+                                  "0123456789";
+            string rngValue = "";
 
-            byte[] rngSecret = new byte[20];
-
-
-            if (mPassword.Equals(Client.masterPassword))
+            
+            if (mPassword.Equals(Client.masterPassword) && flag == null)
             {
                 Console.WriteLine("Enter the password you would like to keep");
                 string value =  Console.ReadLine();
-            
+                
                 Dictionary<string, string> vault = s.GetUnEncryptedVault();
 
                 vault.Add(property, value);
 
                 s.EncryptVaultAndWriteToServer(vault);
+            } else if (mPassword.Equals(Client.masterPassword) && flag == "-g")
+            {
+                while (rngValue.Length != 20)
+                {
+                    byte[] rngChar = new byte[1];
+                    rng.GetBytes(rngChar);
+                    char character = (char)rngChar[0];
+                    if (alphaNumeric.Contains(character))
+                    {
+                        rngValue += character;
+                    }
+                }
+                Console.WriteLine(rngValue);
             } else
             {
                 Console.WriteLine("Wrong password, command aborted");
